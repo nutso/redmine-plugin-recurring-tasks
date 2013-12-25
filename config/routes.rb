167d82ab532675db8500ba44ab7c6af3f90689e5
Teresa <teresa.nuzzo@gmail.com>
2013-12-25 15:56:08 -0500

    missing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
-match 'recurring_tasks', :to => 'recurring_tasks#index'
-match 'recurring_tasks/new', :to => 'recurring_tasks#new'
-match 'recurring_tasks/create', :to => 'recurring_tasks#create', :via => [:post]
-match 'recurring_tasks/:id/show', :to => 'recurring_tasks#show'
-match 'recurring_tasks/:id/edit', :to => 'recurring_tasks#edit'
-match 'recurring_tasks/:id/update', :to => 'recurring_tasks#update', :via => [:patch, :put]
-match 'recurring_tasks/:id/destroy', :to => 'recurring_tasks#destroy', :via => [:delete]
+resources :recurring_tasks
+
+# match 'recurring_tasks', :to => 'recurring_tasks#index'
+# match 'recurring_tasks/new', :to => 'recurring_tasks#new'
+# match 'recurring_tasks/create', :to => 'recurring_tasks#create', :via => [:post]
+# match 'recurring_tasks/:id/show', :to => 'recurring_tasks#show'
+# match 'recurring_tasks/:id/edit', :to => 'recurring_tasks#edit'
+# match 'recurring_tasks/:id/update', :to => 'recurring_tasks#update', :via => [:patch, :put]
+# match 'recurring_tasks/:id/destroy', :to => 'recurring_tasks#destroy', :via => [:delete]
