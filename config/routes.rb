c5e412f190e4e3c5f28559740607c8931746152e
Teresa <teresa.nuzzo@gmail.com>
2013-12-28 16:17:19 -0500

    #11 no longer taking over project routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
-resources :projects do
-  resources :recurring_tasks
-end
+resources :recurring_tasks
+
 # another way to view recurring tasks
-#match 'projects/:project_id/recurring_tasks', :to => 'recurring_tasks#index'
-#match 'projects/:project_id/recurring_tasks/:id', :to => 'recurring_tasks#show'
-match 'recurring_tasks', :to => 'recurring_tasks#index'
+match 'projects/:project_id/recurring_tasks', :to => 'recurring_tasks#index'
+match 'projects/:project_id/recurring_tasks/:id', :to => 'recurring_tasks#show'
