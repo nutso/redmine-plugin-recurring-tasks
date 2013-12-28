4a98dca13846c1a8effa281f7201d72346ec7dfd
Teresa <teresa.nuzzo@gmail.com>
2013-12-28 16:14:33 -0500

    #11 displaying links only as allowed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
-resources :recurring_tasks
-
+resources :projects do
+  resources :recurring_tasks
+end
 # another way to view recurring tasks
-match 'projects/:project_id/recurring_tasks', :to => 'recurring_tasks#index'
+#match 'projects/:project_id/recurring_tasks', :to => 'recurring_tasks#index'
+#match 'projects/:project_id/recurring_tasks/:id', :to => 'recurring_tasks#show'
+match 'recurring_tasks', :to => 'recurring_tasks#index'
