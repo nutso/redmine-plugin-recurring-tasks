38241a7d233379094a421c34f47d141833865dc8
Teresa <teresa.nuzzo@gmail.com>
2013-12-29 18:41:43 -0500

    passing all parameters

--- app/helpers/recurring_tasks_helper.rb
+++ app/helpers/recurring_tasks_helper.rb
@@ -6,13 +6,13 @@ module RecurringTasksHelper
   
   def delete_button recurring_task
     if User.current.allowed_to?(:delete_issue_recurrence, recurring_task.project)
-      button_to(l(:button_delete), {:action => 'destroy', :id => recurring_task}, :method => :delete, :class => 'icon icon-del', :confirm => l(:text_are_you_sure))
+      button_to(l(:button_delete), {:action => 'destroy', :id => recurring_task, :project_id => recurring_task.project.id}, :method => :delete, :class => 'icon icon-del', :confirm => l(:text_are_you_sure))
     end
   end
   
   def edit_button recurring_task
     if User.current.allowed_to?(:edit_issue_recurrence, recurring_task.project)
-      link_to(l(:button_edit), {:action => 'edit', :id => recurring_task}, :class => 'icon icon-edit')
+      link_to(l(:button_edit), {:action => 'edit', :id => recurring_task, :project_id => recurring_task.project.id}, :class => 'icon icon-edit')
     end
   end
 end
