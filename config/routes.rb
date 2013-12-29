0e06d553ca48612dc6edb58de181147fdb91f977
Teresa <teresa.nuzzo@gmail.com>
2013-12-29 16:07:40 -0500

    #7 named route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ match 'recurring_tasks', :to => 'recurring_tasks#index'
 
 # project view 
 match 'projects/:project_id/recurring_tasks', :to => 'recurring_tasks#index'
-match 'projects/:project_id/recurring_tasks/new(/:issue_id)', :to => 'recurring_tasks#new', :as => :new_recurring_task
+match 'projects/:project_id/recurring_tasks/new/:issue_id', :to => 'recurring_tasks#new', :as => :new_recurring_task
 match 'projects/:project_id/recurring_tasks/create', :to => 'recurring_tasks#create'
 match 'projects/:project_id/recurring_tasks/:id', :to => 'recurring_tasks#show'
 match 'projects/:project_id/recurring_tasks/:id/edit', :to => 'recurring_tasks#edit'
