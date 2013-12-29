ebd06ad1b5cb15f3fd378e9248164b88e3ac8967
Teresa <teresa.nuzzo@gmail.com>
2013-12-29 15:59:56 -0500

    #7 passing with new issue

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ match 'recurring_tasks', :to => 'recurring_tasks#index'
 
 # project view 
 match 'projects/:project_id/recurring_tasks', :to => 'recurring_tasks#index'
-match 'projects/:project_id/recurring_tasks/new', :to => 'recurring_tasks#new'
+match 'projects/:project_id/recurring_tasks/new/:issue', :to => 'recurring_tasks#new'
 match 'projects/:project_id/recurring_tasks/create', :to => 'recurring_tasks#create'
 match 'projects/:project_id/recurring_tasks/:id', :to => 'recurring_tasks#show'
 match 'projects/:project_id/recurring_tasks/:id/edit', :to => 'recurring_tasks#edit'
