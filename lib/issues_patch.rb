c161c0f0b7f7437ced6bfd8cd723a6f0a75e6542
Teresa <teresa.nuzzo@gmail.com>
2014-01-01 11:38:52 -0500

    catching error when issue is deleted even though recurrence still exists

--- lib/issues_patch.rb
+++ lib/issues_patch.rb
@@ -4,7 +4,12 @@ module RecurringTasks
       base.class_eval do
         # adding a property to issues that shows subject (date) that can be used in selecting issues
         def subj_date
-          "#{self.subject} (#{format_date self.due_date})"
+          begin
+            "#{self.subject} (#{format_date self.due_date})"
+          rescue
+            # TODO log info or whatnot if self is nil ... for some reason it still gets here
+            ""
+          end
         end #subj_date
         
         # whether this issue recurs
