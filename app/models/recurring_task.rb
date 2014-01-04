cd938228838e96419211f3e6df77cd4dae506298
Teresa <teresa.nuzzo@gmail.com>
2014-01-04 15:08:22 -0500

    more backward compatibility

--- app/models/recurring_task.rb
+++ app/models/recurring_task.rb
@@ -14,8 +14,13 @@ class RecurringTask < ActiveRecord::Base
   # must come before validations otherwise uninitialized
   INTERVAL_UNITS_LOCALIZED = [l(:interval_day), l(:interval_week), l(:interval_month), l(:interval_year)]
 
-  validates :interval_localized_name, presence: true, inclusion: { in: RecurringTask::INTERVAL_UNITS_LOCALIZED, message: "#{l(:error_invalid_interval)} '%{value}' (Validation)" }
-  validates :interval_number, presence: true, numericality: {only_integer: true, greater_than: 0}
+  # pulled out validates_presence_of separately
+  # for older Rails compatibility
+  validates_presence_of :interval_localized_name
+  validates_presence_of :interval_number
+  
+  validates :interval_localized_name, inclusion: { in: RecurringTask::INTERVAL_UNITS_LOCALIZED, message: "#{l(:error_invalid_interval)} '%{value}' (Validation)" }
+  validates :interval_number, numericality: {only_integer: true, greater_than: 0}
   # cannot validate presence of issue if want to use other features; requiring presence of fixed_schedule requires it to be true
 
   validates_associated :issue # just in case we build in functionality to add an issue at the same time, verify the issue is ok  
