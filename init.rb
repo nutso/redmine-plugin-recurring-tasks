883e61698f8ce0eae2018c4ade58448d00b08037
Teresa <teresa.nuzzo@gmail.com>
2015-06-14 11:48:33 -0400

    #54 updated proc to check for setting value

--- init.rb
+++ init.rb
@@ -14,12 +14,12 @@ Redmine::Plugin.register :recurring_tasks do
   
   # user-accessible global configuration
   settings :default => {
-    'show_top_menu' => true
+    'show_top_menu' => "1"
   }, :partial => 'settings/recurring_tasks_settings'
 
   
   Redmine::MenuManager.map :top_menu do |menu|
-    menu.push :recurring_tasks, { :controller => 'recurring_tasks', :action => 'index' }, :caption => :label_recurring_tasks, :if => Proc.new { User.current.admin? }
+    menu.push :recurring_tasks, { :controller => 'recurring_tasks', :action => 'index' }, :caption => :label_recurring_tasks, :if => Proc.new { User.current.admin? && (Setting.plugin_recurring_tasks['show_top_menu'] == "1")}
   end
   
   # Permissions map to issue permissions (#12)
