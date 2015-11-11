a2159ffbdbe2a470bd67105581be3e0ad7997798
Bart van Zon <bartvanzon@gmail.com>
2015-11-11 13:24:20 +0100

    Rename descendants to subtasks and fix subtask dates
    Renamed the descendants value because the option is called subtasks as
    well, and descendants is actually incorrect as the copy function works
    with children, not descendants.
    
    Fixes subtask dates to have the correct start & due date, and save the
    parent task a second time after setting the subtasks to force the proper
    date. Because changing subtasks dates change the parent task dates.

--- app/models/recurring_task.rb
+++ app/models/recurring_task.rb
@@ -4,7 +4,7 @@ class RecurringTask < ActiveRecord::Base
   belongs_to :issue, :foreign_key => 'current_issue_id'
   has_one :project, :through => :issue
 
-  attr_accessible :id, :current_issue_id, :interval_number, :interval_modifier, :interval_unit, :fixed_schedule, :recur_descendants # list all fields that you want to be accessible here
+  attr_accessible :id, :current_issue_id, :interval_number, :interval_modifier, :interval_unit, :fixed_schedule, :recur_subtasks# list all fields that you want to be accessible here
   
   # these are the flags used in the database to denote the interval
   # the actual text displayed to the user is controlled in the language file
@@ -271,25 +271,29 @@ class RecurringTask < ActiveRecord::Base
 
       if Setting.plugin_recurring_tasks['reopen_issue'] != "1"
         # duplicate issue; cloning comes after setting the user so the author is set correctly (#89)
-        new_issue = issue.copy nil, :subtasks => recur_descendants?
+        new_issue = issue.copy nil, :subtasks => recur_subtasks?
+        new_issue.save!
       end
 
+      if recur_subtasks?
+        new_issue.reload.children.each do |subtask|
+          subtask_offset = (next_scheduled_recurrence - timespan) - issue.start_date
+          subtask_timespan = subtask.due_date - subtask.start_date rescue 0
+          subtask.start_date = subtask.start_date + subtask_offset
+          subtask.due_date = subtask.start_date + subtask_timespan
+          subtask.done_ratio = 0
+          subtask.status = recurring_issue_default_status
+          subtask.save!
+        end
+
+        new_issue.reload
+      end
 
       new_issue.due_date = next_scheduled_recurrence #41 previous_date_for_recurrence + recurrence_pattern
       new_issue.start_date = new_issue.due_date - timespan
       new_issue.done_ratio = 0
       new_issue.status = recurring_issue_default_status
       new_issue.save!
-
-      if recur_descendants?
-        new_issue.reload.descendants.each do |descendant|
-          descendant.due_date = next_scheduled_recurrence
-          descendant.start_date = new_issue.due_date - timespan
-          descendant.done_ratio = 0
-          descendant.status = recurring_issue_default_status
-          descendant.save
-        end
-      end
 
       puts "#{l(:recurring_task_created)} #{issue.id}: #{issue.subj_date} => #{new_issue.id}: #{new_issue.subj_date}"
     
--- db/migrate/005_add_recur_descendants_field.rb
+++
@@ -1,7 +0,0 @@
-class AddRecurDescendantsField < ActiveRecord::Migration
-  def change
-    change_table :recurring_tasks do |t|
-      t.column :recur_descendants, :boolean
-    end
-  end
-end
